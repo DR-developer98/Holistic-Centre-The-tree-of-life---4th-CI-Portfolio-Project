d37f7c04f8476def52550ae55690f14c172fc0cb
DR-developer98 <xhfnpf57@students.codeinstitute.net>
2025-07-16 20:39:36 +0200

    Import cloudinary, create Employee model and register it in the admin panel

--- landing_page/models.py
+++ landing_page/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Employee(models.Model):
+
+    featured_image = CloudinaryField('image', default='placeholder')
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+	
+    def __str__ (self):
+	    return self.name
+
--- tolcentre/settings.py
+++ tolcentre/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wp_%-zao2la_q@+x1w5)j&wo*emcs$m&jhqpg+&if&4x^0jdxo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'landing_page',
 ]
 
