02060884a14ac8440b222ccfbc3096db1ea1f614
DR-developer98 <xhfnpf57@students.codeinstitute.net>
2025-07-20 18:21:07 +0200

    Create treatments app and respective model and migrate it to database

--- home/admin.py
+++ home/admin.py
@@ -3,4 +3,10 @@ from django_summernote.admin import SummernoteModelAdmin
 from .models import Employee
 
 # Register your models here.
-admin.site.register(Employee)
+@admin.register(Employee)
+class EmployeeAdmin(SummernoteModelAdmin):
+
+    list_display = ('name', 'last_name', 'slug')
+    search_fields = ['name', 'last_name']
+    prepopulated_fields = {'slug': ('name', 'last_name')}
+    summernote_fields = ('bio',)
---
+++ treatments/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from home.models import Employee
+
+# Create your models here.
+class Treatment(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.DurationField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    practitioner = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="treatments")
+
+    def __str__ (self):
+        return self.name
