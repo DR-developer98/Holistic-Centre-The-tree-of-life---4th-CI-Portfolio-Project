602f1e9bcd3f34c805ebf2b886cc5267f8c24121
DR-developer98 <xhfnpf57@students.codeinstitute.net>
2025-07-31 19:31:22 +0200

    Fix appointment app level urls to enable appointment cancellation

--- tolcentre/urls.py
+++ tolcentre/urls.py
@@ -17,15 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('', include("home.urls"), name="home_urls"),
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
+    path('appointments/', include('appointments.urls'), 
+         name="appointments_urls"),
     path('contact/', include("get_in_touch.urls"), name="get_in_touch_urls"),
-    path('makeappointment/', include('appointments.urls'),
-         name="appointments_urls"),
-    path('myappointments/', include('appointments.urls'),
-         name="appointments_urls"),
     path('summernote/', include('django_summernote.urls')),
     path('treatments/', include("treatments.urls"), name="treatments.urls"),
 ]
